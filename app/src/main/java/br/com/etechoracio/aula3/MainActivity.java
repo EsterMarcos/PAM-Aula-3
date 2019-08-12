22e2fcb9a28cda3da048d6b4875b836f0f4a7713
EsterMarcos <estermarcoshas@gmail.com>
2019-08-12 20:45:59 -0300

    Pam Aula 3

---
+++ app/src/main/java/br/com/etechoracio/aula3/MainActivity.java
@@ -0,0 +1,27 @@
+package br.com.etechoracio.aula3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onProcessar (View view)
+    {
+
+        Toast msg = Toast.makeText(this ,"Erro no carregamento",Toast.LENGTH_LONG);
+        msg.show();
+
+    }
+}
+
+
+
+
